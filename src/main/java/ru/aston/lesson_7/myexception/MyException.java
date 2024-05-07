e051d005c7b171f3bf991ba2ce0c3ea8bac59f73
BoneSeller <d.oreh@mail.ru>
2024-05-07 18:13:01 +0300

    Complete lesson_8

--- src/main/java/ru/aston/lesson_7/myexception/MyException.java
+++
@@ -1,9 +0,0 @@
-package ru.aston.lesson_7.myexception;
-
-public class MyException extends RuntimeException{
-    private String message;
-
-    public MyException(String message) {
-        super(message);
-    }
-}
